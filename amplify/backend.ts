8b58cad86b18272c49bafdfc76e52eda436b2d7a
Junior-Weil <rmw020@bucknell.edu>
2025-09-22 16:02:08 -0500

    adding some lambda stuff, this is just a test push after changing repo origin url

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+// import { NeuralNetwork } from './functions/NeuralNetwork/resources';
 
 defineBackend({
   auth,
   data,
+  // NeuralNetwork,
 });
