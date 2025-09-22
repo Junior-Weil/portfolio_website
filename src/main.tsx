932affa07c0e57d4e428054eb15323ce0c1df61a
Junior-Weil <rmw020@bucknell.edu>
2025-09-22 17:52:44 -0500

    uncommenting strict mode in main and removing imports causing failed deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
@@ -10,9 +9,7 @@ import '@aws-amplify/ui-react/styles.css';
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-    // <Authenticator >
+  <React.StrictMode>
       <App />
-    // </Authenticator >
-   /* </React.StrictMode> */
+   </React.StrictMode>
 );
